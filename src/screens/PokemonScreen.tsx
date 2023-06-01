a97741273366a0504306b6214a993759af91c713
Alejo Di Loreto <alejodiloreto1@gmail.com>
2023-06-01 19:37:01 -0300

    detail screen

--- src/screens/PokemonScreen.tsx
+++ src/screens/PokemonScreen.tsx
@@ -1,18 +1,83 @@
-import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { TouchableOpacity, View, StyleSheet, Text, Image } from 'react-native'
 import { RootStackParams } from '../navigation/StackNavigator'
+import Icon from 'react-native-vector-icons/Ionicons';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { FadeInImage } from '../components/FadeInImage';
 
 interface Props extends NativeStackScreenProps<RootStackParams, 'PokemonScreen'> { }
 
 const PokemonScreen = ({ navigation, route }: Props) => {
 
+  const { top } = useSafeAreaInsets();
   const { pokemon, color } = route.params;
+  const { id, name, picture } = pokemon
+
   return (
     <View>
-      <Text style={{ color }}>{pokemon.name}</Text>
+      <View style={
+        [styles.headerContainer, { backgroundColor: color }]
+      }>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          activeOpacity={.8}
+          style={[styles.backButton, { top: top + 10 }]}
+        >
+          <Icon
+            name='arrow-back-outline'
+            color='white'
+            size={35}
+          />
+        </TouchableOpacity>
+        <Text
+          style={[styles.pokemonName, { top: top + 40 }]}
+        >
+          {name + '\n'}#{id}
+        </Text>
+        <Image
+          source={require('../assets/white-pokeball.png')}
+          style={styles.pokeball}
+        />
+        <FadeInImage
+          uri={picture}
+          style={styles.pokemonImage}
+        />
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    height: 370,
+    zIndex: 999,
+    alignItems: 'center',
+    borderBottomRightRadius: 1000,
+    borderBottomLeftRadius: 1000
+  },
+  backButton: {
+    position: 'absolute',
+    left: 20,
+  },
+  pokemonName: {
+    color: 'white',
+    fontSize: 40,
+    alignSelf: 'flex-start',
+    left: 20,
+  },
+  pokeball: {
+    width: 250,
+    height: 250,
+    bottom: -15,
+    opacity: .7
+  },
+  pokemonImage: {
+    width: 250,
+    height: 250,
+    position: 'absolute',
+    bottom: -15
+  }
+})
 
 export default PokemonScreen
